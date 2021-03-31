9e487a7e38b8fa1acbf284a911482d86d046b387
Kulbhushan <kulbhushan.mayer@>
2021-03-31 12:19:49 +0530

    updated string

--- routes/employees.js
+++ routes/employees.js
@@ -1,5 +1,5 @@
 const { Client } = require('pg');
-var connectionString = "postgres://"+DB_USER+":"+DB_PWD+"@"+DB_URL+"/nodejs_demo";
+var connectionString = "postgres://"+process.env.DB_USER+":"+process.env.DB_PWD+"@"+process.env.DB_URL+"/nodejs_demo";
 const client = new Client({
     connectionString: connectionString
 });
