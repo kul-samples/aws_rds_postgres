24c3e79724db50b67017fdbc4e6ad13d4ea74da2
Kulbhushan <kulbhushan.mayer@>
2021-05-16 18:03:55 +0530

    added selenium test code

---
+++ test_app.py
@@ -0,0 +1,34 @@
+import time
+from time import sleep
+from unittest import TestCase
+from selenium import webdriver
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
+
+import config
+
+class TestVotePage(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(ChromeDriverManager().install())
+        cls.driver.get(config.server)
+        time.sleep(3)
+
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
+
+
+    def test01_page_title(self):
+        self.assertEqual("NodeJs & PostgreSQL CRUD Application",self.driver.title)
+        # assert True
+
+    def test02_form_elements_present(self):
+        self.assertTrue(self.driver.find_element_by_link_text("View or Add Employees Details here!"))
+        
