533797f00a86612aac42a943bb883d9b6be86c93
Kulbhushan Mayer <kulbhushan.mayer@gmail.com>
2021-10-14 17:18:28 +0530

    Create database-setup.sh

---
+++ manifests/database-setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+psql -h $DB_URL -U $DB_USER -tc "SELECT 1 FROM pg_database WHERE datname = 'nodejs_demo'" | grep -q 1 | psql -h $DB_URL -U $DB_USER --dbname postgres -c "CREATE DATABASE nodejs_demo"
+psql -h $DB_URL -U $DB_USER --dbname nodejs_demo < create-table.sql
